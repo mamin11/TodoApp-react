b76295514211e08eb36766bef013091370451bd1
mamin11 <abdimamin@gmail.com>
2020-10-11 14:05:07 +0100

    Updates

---
+++ precache-manifest.fdf68a31023892cbbb8e74e2e1da99ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05db44ebd1ad44c34ae86144c2eedc17",
+    "url": "/todoApp-react/index.html"
+  },
+  {
+    "revision": "a9bbb4fd163fe06b764b",
+    "url": "/todoApp-react/static/css/main.26a1d012.chunk.css"
+  },
+  {
+    "revision": "4669181bda53cc858240",
+    "url": "/todoApp-react/static/js/2.c3b95528.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todoApp-react/static/js/2.c3b95528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9bbb4fd163fe06b764b",
+    "url": "/todoApp-react/static/js/main.120a69b2.chunk.js"
+  },
+  {
+    "revision": "a3243a420761159b07f8",
+    "url": "/todoApp-react/static/js/runtime-main.57f3375f.js"
+  }
+]);
